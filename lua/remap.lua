1b481a81ca7d7d2ccc8b76a9764d87255f7e16e5
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-11-26 12:46:23 +0100

    minor fixes

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', "<leader>w", "mzgg=G`z<cmd>w<CR>", { desc="Format and save" })
-vim.keymap.set("n","<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gl<Left><Left><Left>", { desc="replace current word" })
+vim.keymap.set("n", "<leader>w", "mzgg=G`z<cmd>w<CR>", { desc = "Format and save" })
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gl<Left><Left><Left>", { desc = "replace current word" })
