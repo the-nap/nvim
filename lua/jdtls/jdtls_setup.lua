58d4ca7037e9c3e51a35c8e5b439a822db3e618a
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-12-04 10:40:18 +0100

    moved absolute to relative paths

--- lua/jdtls/jdtls_setup.lua
+++ lua/jdtls/jdtls_setup.lua
@@ -1,41 +1,57 @@
 local M = {}
 
-function M:setup()
+function M.setup()
+  local home = os.getenv('HOME')
+
   -- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+  local jdtls_root = home .. '/.local/share/nvim/mason/packages/jdtls/'
 
-  local workspace_dir = '/home/thecoder_/development/jdtls_data/' .. project_name
+  local workspace_dir = home .. '/development/jdtls_data/' .. project_name
+  local lombok_jar = jdtls_root .. 'lombok.jar'
+
+  local status, jdtls = pcall(require, 'jdtls')
+  if not status then
+    return
+  end
+
+  -- Additional capabilities
+  local extendedClientCapabilities = jdtls.extendedClientCapabilities
+  extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
   local config = {
     cmd = {
-      "java", --no jdk specifically needed because jdk 25 is the only one installed
+      'java',
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.level=ALL',
+      '-Xmx1G',
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens',
+      'java.base/java.util=ALL-UNNAMED',
+      '--add-opens',
+      'java.base/java.lang=ALL-UNNAMED',
 
-      -- random shit eclipse wants me to write
-      "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-      "-Dosgi.bundles.defaultStartLevel=4",
-      "-Declipse.product=org.eclipse.jdt.ls.core.product",
-      "-Dlog.level=ALL",
-      "-Xmx1G",
-      "--add-modules=ALL-SYSTEM",
-      "--add-opens java.base/java.util=ALL-UNNAMED",
-      "--add-opens java.base/java.lang=ALL-UNNAMED",
-      -- end of random shit
+      --lombok
+      '-javaagent:' .. lombok_jar,
 
 
       "-jar",
-      "/home/thecoder_/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
-      --  ^^^^^^^^^^ 
+      vim.fn.glob(jdtls_root .. 'plugins/org.eclipse.equinox.launcher_*.jar'),
+      --                           ^^^^^^^^^^ 
       -- points to the eclipse.jdt.ls installation
 
       "-configuration",
-      "/home/thecoder_/.local/share/nvim/mason/packages/jdtls/config_linux",
-      --  ^^^^^^^^^^                                               ^^^^^^^^^^^
-      -- points to eclips.jdt.ls installation                  needs to know your os
+      jdtls_root .. 'config_linux',
+      --              ^^^^^^^^^^
+      -- needs to know your os
 
 
       -- needs some place to store index data for each project it loads
       -- you don't want it to decide by himself
       '-data', workspace_dir,
-      '-javaagent:' .. '/home/thecoder_/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+
     },
 
     -- root_dir points to root of project, it is identified by the presence of one of those files
@@ -43,14 +59,29 @@ function M:setup()
 
     -- can configure jdtls for specific needs, can cry here later
     settings = {
-      java = {}
+      java = {
+        signatureHelp = {
+          enabled = true
+        },
+        extendedClientCapabilities = extendedClientCapabilities,
+        configuration = {
+          configuration = {
+            updateBuildConfiguration = "interactive",
+          },
+          maven = {
+            downloadSources = true,
+          },
+          lombok = {
+            lombokEnabled = true,
+          }
+        }
+      },
     },
 
-    -- same but with initialization options, like debugger
-    init_options = {
-      bundles = {}
-    },
-  }
+      -- same but with initialization options, like debugger
+      init_options = {
+      },
+    }
 
   -- Start or attach already existent client and server
   require('jdtls').start_or_attach(config)
